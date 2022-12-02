2135935c425006934732eef660f5f89001dcd240
miguelcruz <miguelcocruz@gmail.com>
2022-12-02 22:05:28 +0000

    Changed dsn

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -3,10 +3,10 @@ import psycopg2 as ps
 
 
 def connect():
-    con = ps.connect(
-        user="root",
-        password="example",
-        host="db",
-        port=5432
-    )
+    user = "root"
+    password = "example"
+    host = "db"
+    port = 5432
+
+    con = ps.connect(f'postgresql://{user}:{password}@{host}:{port}/')
     return con
